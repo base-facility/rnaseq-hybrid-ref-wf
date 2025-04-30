e26baca0d14cae1bccee981af2a6aaa0210e5944
moe.z <mzed@pm.me>
2025-05-01 09:38:55 +1000

    testing scripts modified

--- workflow/scripts/checkQuant.py
+++ workflow/scripts/checkQuant.py
@@ -7,7 +7,7 @@ quant_sf = sys.argv[1]
 # df = pd.read_csv(quant_sf, header=None, names=["Name", "Length", "EffectiveLength", "TPM", "NumReads"], sep="\t")
 df = pd.read_csv(quant_sf, sep="\t")
 # print(df.head())
-to_filter = ["ENST00000354866.7", "ENST00000262320.8", "ENST00000646664.1", "ENST00000269305.9", "synth_axin1", "synth_tp53"]
+to_filter = ["ENST00000354866.7", "ENST00000262320.8", "ENST00000646664.1", "ENST00000269305.9", "synth_axin1", "synth_tp53", "synth_fluc"]
 rename_dict = {
             "ENST00000354866.7": "AXIN-202",
             "ENST00000262320.8": "AXIN-201",
@@ -19,7 +19,7 @@ plot_df['Name'] = plot_df['Name'].replace(rename_dict)
 print(plot_df.head(50))
 # print(stats.describe(df['TPM']))
 # print(df['TPM'].describe())
-print(df.iloc[df['TPM'].idxmax()])
+# print(df.iloc[df['TPM'].idxmax()])
 print(df.nlargest(10, 'TPM'))
 # plt.plot(df["Name"], df["TPM"])
 # plt.savefig("tpm_plot.png")
